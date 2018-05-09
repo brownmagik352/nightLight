a3222e7e09246467688a13330e5cc2dc0cb9c688
Apurv Suman <sumanicus@gmail.com>
2018-05-09 11:04:23 -0700

    swapped in RGB-LED pins for single pin in scaffolding-basic

--- A03-BLEBasic/RedBearDuoBLEBasic/RedBearDuoBLEBasic.ino
+++ A03-BLEBasic/RedBearDuoBLEBasic/RedBearDuoBLEBasic.ino
@@ -21,10 +21,24 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 #define BLE_SHORT_NAME_LEN 0x08 // must be in the range of [0x01, 0x09]
 #define BLE_SHORT_NAME 'a','p','s','u','m','a','n'  // define each char but the number of char should be BLE_SHORT_NAME_LEN-1
 
-/* Define the pins on the Duo board
- * TODO: change the pins here for your applications
- */
-#define DIGITAL_OUT_PIN            D2
+/* Define the pins on the Duo board */
+#define COMMON_ANODE 1
+
+const int POT_INPUT_PIN = A0;
+
+const int PHOTO_INPUT_PIN = A4;
+const float MAX_VOLTAGE = 3.3;
+const int MAX_ADC_VALUE = 4096;
+
+const int RGB_RED_PIN = D0;
+const int RGB_GREEN_PIN  = D1;
+const int RGB_BLUE_PIN  = D2;
+const int DELAY = 500; // delay between changing colors
+
+// struct to store rgb values
+struct RGB {
+  int r, g, b;
+};
 
 
 // UUID is used to find the device by other BLE-abled devices
@@ -73,18 +87,12 @@ int bleWriteCallback(uint16_t value_handle, uint8_t *buffer, uint16_t size) {
     }
     Serial.println(" ");
     
-    /* Process the data
-     * TODO: Receive the data sent from other BLE-abled devices (e.g., Android app)
-     * and process the data for different purposes (digital write, digital read, analog read, PWM write)
-     */
+    /* Process the data */
     if (receive_data[0] == 0x01) { // Command is to control digital out pin
       if (receive_data[1] == 0x01)
-        digitalWrite(DIGITAL_OUT_PIN, HIGH);
+        setColor(255, 0, 0); // red
       else
-        digitalWrite(DIGITAL_OUT_PIN, LOW);
-    }
-    else if (receive_data[0] == 0x04) { // Command is to initialize all.
-      digitalWrite(DIGITAL_OUT_PIN, LOW);
+        setColor(0, 0, 255); // blue
     }
   }
   return 0;
@@ -112,10 +120,62 @@ void setup() {
   ble.startAdvertising();
   Serial.println("BLE start advertising.");
 
-  /*
-   * TODO: This is where you can initialize all peripheral/pin modes
-   */
-  pinMode(DIGITAL_OUT_PIN, OUTPUT);
+  /* initialize all peripheral/pin modes */
+  pinMode(POT_INPUT_PIN, INPUT);
+
+  pinMode(PHOTO_INPUT_PIN, INPUT);
+  
+  pinMode(RGB_RED_PIN, OUTPUT);
+  pinMode(RGB_GREEN_PIN, OUTPUT);
+  pinMode(RGB_BLUE_PIN, OUTPUT);
 }
 
 void loop() {}
+
+// *** HELPER/PROCESSING FUNCTIONS *** //
+
+//function that goes from single pot value to hex color
+unsigned int potToHex(int potVal) {
+  unsigned int hexVal;
+  hexVal = map(potVal, 0, 4092, 0, 256 * 256 * 256);
+  return hexVal;
+}
+
+// function that goes from hex color value to rgb
+struct RGB hexToRGB(unsigned int hexVal) {
+  struct RGB ledVal; 
+
+  // try this for hex to rgb: https://stackoverflow.com/questions/3723846/convert-from-hex-color-to-rgb-struct-in-c
+  ledVal.r = (hexVal >> 16) & 0xFF;
+  ledVal.g = (hexVal >> 8) & 0xFF;
+  ledVal.b = (hexVal) & 0xFF;
+
+  return ledVal;
+}
+
+// function that takes an RGB LED value and modifies brightness based on photovoltaic cell
+struct RGB rgbLedBrightnessAdjuster(struct RGB original, int photoVal) {
+  struct RGB brightnessAdjusted;
+
+  // use brightness as the cap on intensity for each of RGB; inverted so darker lighting => brighter LED
+  // based on http://forum.arduino.cc/index.php?topic=272862.0
+  int brightness = map(photoVal, 0, 4096, 255, 0);
+  
+  // now modify RGB with brightness
+  brightnessAdjusted.r = map(original.r, 0, 255, 0, brightness);
+  brightnessAdjusted.g = map(original.g, 0, 255, 0, brightness);
+  brightnessAdjusted.b = map(original.b, 0, 255, 0, brightness);
+  
+  return brightnessAdjusted;
+}
+
+void setColor(int red, int green, int blue) {
+  #ifdef COMMON_ANODE
+    red = 255 - red;
+    green = 255 - green;
+    blue = 255 - blue;
+  #endif
+  analogWrite(RGB_RED_PIN, red);
+  analogWrite(RGB_GREEN_PIN, green);
+  analogWrite(RGB_BLUE_PIN, blue);  
+}
